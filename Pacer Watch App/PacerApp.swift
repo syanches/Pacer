3f9a98fa73f529fdfc9815d3c4787ef2ea35b6a8
Artem Martirosyan <syanches@gmail.com>
2023-01-22 20:30:29 +0300

    Initial Commit

---
+++ Pacer Watch App/PacerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PacerApp.swift
+//  Pacer Watch App
+//
+//  Created by Artem Martirosyan on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Pacer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
