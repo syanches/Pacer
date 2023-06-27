0f9cf275d30be55948fe1879b73e5c0aa30746fc
Artem Martirosyan <syanches@gmail.com>
2023-06-27 23:40:06 +0300

    Testing new DatePicker API

---
+++ Pacer Watch App/TimeView.swift
@@ -0,0 +1,18 @@
+//
+//  TimeView.swift
+//  Pacer Watch App
+//
+//  Created by Artem Martirosyan on 27.06.2023.
+//
+
+import SwiftUI
+
+struct TimeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimeView()
+}
